58230acf991b97404b6430b6230f7941eaee8034
fjuzaino <41808092+fjuzaino@users.noreply.github.com>
2019-05-21 18:51:31 -0700

    Update graph_energy.f90

--- graph_energy.f90
+++ graph_energy.f90
@@ -11,15 +11,15 @@ SUBROUTINE eKU(a,b)
   !i: Contador
   !a: Variable dummy para el protonmovil enviado del programa principal
   !-------------------------------------------
-  REAL::ac,r,Ki,U !aceleración, radio, energía cinetica y potencial respectivamente
+  REAL(d)::ac,r,Ki,U !aceleración, radio, energía cinetica y potencial respectivamente
   TYPE(charged_particles),INTENT(IN)::a !partícula cargada
-  REAL,INTENT::b !!tiempo
+  REAL(d),INTENT(IN)::b !!tiempo
 	
 	!Se abre el archivo donde se tabulará
   OPEN(UNIT=132,FILE="Graph",STATUS="UNKNOWN",ACTION="WRITE")
-  a%vel=a%mom/a%mass !velocidad de la partícula
+ !! a%vel=a%mom/a%mass !velocidad de la partícula
   ac=a%Felec/a%mass
-  Ki=(1/2)*a%mass*a%vel**2 !calculo de la energía cinética
+  Ki=(1/2)*a%mom**2/a%mass !calculo de la energía cinética
   U=a%mass*ac*r !!cambiar esto, por esta formula https://en.wikipedia.org/wiki/Electric_potential_energy
                 !!usar la formula para n-cargas
   WRITE(132,*)b,U,Ki,U+Ki  !tabular la gráfica
