79a95e181f955e6e17609234d79a2a09b95b8728
ismaelcuen <50840492+ismaelcuen@users.noreply.github.com>
2019-05-24 13:50:15 -0700

    Update campoelectrico.f90

--- campoelectrico.f90
+++ campoelectrico.f90
@@ -1,12 +1,9 @@
-SUBROUTINE campoelectrico(a,b)
+SUBROUTINE campoelectrico(a,b,c)
 USE charged_particles
 USE vector_functions
   IMPLICIT NONE
-  REAL,DIMENSION(3)::a,b
- ! protonmovil%Eelect=0.0_d !!No hace falta
-  sim%pos2 = a%pos-b%pos !!Calculando el vector r=r2-r1
-  runit=sim%pos2/mag(sim%pos2)
+  REAL,DIMENSION(3)::a,b,c
   !!Calculando la interacción eléctrica.
-  a%Eelec=( k*a%q / mag(sim%pos2)**2)*runit
+  a%Eelec=(k*a%q/mag(c)**2)*runit
   a%Felec = a%Felec+a%q*a%Eelec
 END SUBROUTINE campoelectrico
