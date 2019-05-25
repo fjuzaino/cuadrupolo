0dabbdcb00d4a79e203ecd9f92fbdd1f5cb2a9da
fjuzaino <41808092+fjuzaino@users.noreply.github.com>
2019-05-25 16:04:51 -0700

    Update campoelectrico.f90

--- campoelectrico.f90
+++ campoelectrico.f90
@@ -1,9 +1,14 @@
-SUBROUTINE campoelectrico(a,b,c)
-USE charged_particles
+SUBROUTINE campoelectrico(a,b)
+USE charged_objects
 USE vector_functions
+
   IMPLICIT NONE
-  REAL,DIMENSION(3)::a,b,c
-  !!Calculando la interacción eléctrica.
-  a%Eelec=(k*a%q/mag(c)**2)*runit
-  a%Felec = a%Felec+a%q*a%Eelec
+
+	TYPE(charged_particles),INTENT(INOUT)::a
+  	REAL(d),INTENT(IN)::b
+
+  	!!Calculando la interacción eléctrica.
+  	a%Eelec=(k*a%q/b**2)*runit
+ 	a%Felec = a%q*a%Eelec
+
 END SUBROUTINE campoelectrico
