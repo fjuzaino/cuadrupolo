2d53e33685868ba33c46d3b94b209613b9ea42e0
fjuzaino <41808092+fjuzaino@users.noreply.github.com>
2019-05-25 16:08:47 -0700

    Update charged_objects.f90

--- charged_objects.f90
+++ charged_objects.f90
@@ -9,7 +9,8 @@ INTEGER , PARAMETER :: d = SELECTED_REAL_KIND ( 16 )
 !!Introduciendo las unidades naturales del sistema
 REAL(d),PARAMETER::pi=3.14159265358979_d !!Pi con este Kind
 REAL(d),PARAMETER::k=9E9_d !!K con este Kind
-REAL(d),DIMENSION(3):: runit
+REAL(d),DIMENSION(3)::runit
+INTEGER::unit1
 
 !!Tipo publico para producir objetos del tipo planeta
 TYPE,PUBLIC::charged_particles
@@ -18,6 +19,8 @@ REAL(d),DIMENSION(3)::mom !!Momento Lineal en el tiempo t
 REAL(d),DIMENSION(3)::vel !!Velocidad en el tiempo t
 REAL(d),DIMENSION(3)::Felec !!Fuerza el´ectrica en el tiempo t
 REAL(d),DIMENSION(3)::Eelec !!Campo el´ectrico en el tiempo t
+REAL(d),DIMENSION(3)::Fmagn !!Fuerza magnetica sobre la partícula movil
+REAL(d),DIMENSION(3)::Felma !!Fuerza electromagnética (Fuerza de Lorentz)
 REAL ( d ) :: mass !!Masa de la particula cargada
 REAL ( d ) :: pos0 !!Posici´on inicial de la particula
 REAL ( d ) :: q !!Carga de la particula
@@ -32,8 +35,9 @@ REAL ( d ) :: dt !!Tama~no de paso
 REAL ( d ) :: N_step !!Cantidad de pasos
 REAL ( d ), DIMENSION ( 3 ) :: Fenet !!Fuerza el´ectrica neta
 REAL ( d ), DIMENSION ( 3 ) :: pos2 !!Vector donde se guarda la
+REAL ( d ):: potencial_total !!energía potencial total en un instante t
 !!diferencia de posici´on entre los objetos
-INTEGER::unit1 !!unidad donde se escribirá la energía potenacial y cinética
+!INTEGER::unit1 !!unidad donde se escribirá la energía potenacial y cinética
 END TYPE data
 
 END MODULE charged_objects
