d92fd51ae26bfe06c39fa260f8a89b6fc3eee7c2
fjuzaino <41808092+fjuzaino@users.noreply.github.com>
2019-05-25 16:21:40 -0700

    Update graph_energy.f90

--- graph_energy.f90
+++ graph_energy.f90
@@ -1,26 +1,27 @@
-SUBROUTINE graph_energy(a,b,r,C)
+SUBROUTINE graph_energy(a,b,c)
   USE charged_objects
   USE vector_functions
   IMPLICIT NONE
   !Ki: Energía Cinética
   !U: Energía Potencial
-  !proton3%mass: Masa
-  !ac: Aceleración
+  !a%mass: Masa
   !a: Variable dummy para el protonmovil enviado del programa principal
-  !C: Variable dummy para la carga del antiproton/proton que actúa
   !r: Radio (distancia entre las partículas que interactúan)
   !b: Tiempo
   !-------------------------------------------
-  REAL(d)::r,Ki,U !radio, energía cinetica y potencial respectivamente
+  REAL(d)::Ki,U !radio, energía cinetica y potencial respectivamente
   TYPE(charged_particles),INTENT(IN)::a !partícula cargada
   REAL(d),INTENT(IN)::b !!tiempo
+  TYPE(data),INTENT(IN)::c
+  
 	
-	!Se abre el archivo donde se tabulará
 
-!! a%vel=a%mom/a%mass !velocidad de la partícula
+
+
   Ki=(1.0/2.0)*a%mass*mag(a%mom/a%mass)**2 !calculo de la energía cinética
-  U= k*a%q*C/mag(r)            !energía potencial con n cargas puntuales
+  !  U= r*mag(c%Fenet)            !energía potencial con n cargas puntuales
+  
                 !!usar la formula para n-cargas
-  WRITE(unit1,*) b,U,Ki,U+Ki  !tabular la gráfica
+  WRITE(unit1,*) b, Ki, c%potencial_total, c%potencial_total + Ki  !tabular la gráfica
   
   END SUBROUTINE graph_energy
